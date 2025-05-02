2b3ea4e61238af8251c4bd943eff3b839ce10e0d
Souma18 <22130078@st.hcmuaf.edu.vn>
2025-05-02 09:13:39 +0700

    Delete Flutter directory

--- Flutter/notes_project/android/app/src/main/kotlin/com/example/notes_project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.notes_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Flutter/notes_project/lib/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-class AppRoutes {
-  static const String home = '/';
-  static const String login = '/login';
-  static const String profile = '/profile';
-  static const String register = '/register';
-  static const String forgotPassword = '/forgot-password';
-  static const String noteListScreen = '/note-list';
-}
